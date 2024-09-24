4a429425cbae43df304d5d5c5a9d93e478e98479
ANGGARA <anggarahmatas@gmail.com>
2024-09-24 22:37:00 +0700

    Mini Project day3

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,22 @@ document.getElementById('messageForm').addEventListener('submit', function (even
     const now = new Date();
     document.getElementById('currentTime').textContent = now.toUTCString();
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+   slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
+
+function plusSlides(n) {
+  slideIndex += n;
+  showSlides();
+}
